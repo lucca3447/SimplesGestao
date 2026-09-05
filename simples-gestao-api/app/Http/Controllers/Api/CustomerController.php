7c6edacf07c22f8de98d73c9e1518d0edf11c75c
Lucca <lucca3447@hotmail.com>
2026-09-05 13:37:54 -0300

    feat: implementar controllers para clientes, categorias e produtos

---
+++ simples-gestao-api/app/Http/Controllers/Api/CustomerController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Customer\StoreCustomerRequest;
+use App\Http\Requests\Customer\UpdateCustomerRequest;
+use App\Http\Resources\CustomerResource;
+use App\Models\Customer;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Symfony\Component\HttpFoundation\Response;
+
+class CustomerController extends Controller
+{
+    public function index(Request $request): AnonymousResourceCollection
+    {
+        $query = Customer::query();
+
+        if ($search = $request->input('search')) {
+            $query->where(function ($q) use ($search) {
+                $q->where('name', 'ilike', "%{$search}%")
+                  ->orWhere('email', 'ilike', "%{$search}%")
+                  ->orWhere('cpf_cnpj', 'like', "%{$search}%");
+            });
+        }
+
+        $customers = $query->latest('id')->paginate($request->integer('per_page', 15));
+
+        return CustomerResource::collection($customers);
+    }
+
+    public function store(StoreCustomerRequest $request): JsonResponse
+    {
+        $customer = Customer::create($request->validated());
+
+        return (new CustomerResource($customer))
+            ->response()
+            ->setStatusCode(Response::HTTP_CREATED);
+    }
+
+    public function show(Customer $customer): CustomerResource
+    {
+        return new CustomerResource($customer);
+    }
+
+    public function update(UpdateCustomerRequest $request, Customer $customer): CustomerResource
+    {
+        $customer->update($request->validated());
+
+        return new CustomerResource($customer);
+    }
+
+    public function destroy(Customer $customer): Response
+    {
+        $customer->delete();
+
+        return response()->noContent();
+    }
+}
